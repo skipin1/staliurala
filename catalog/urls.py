3536d429f956a1c9e7fda8f236c135da94c08992
Sergey Lutkov <lutkovsv@gmail.com>
2014-07-20 17:18:37 +0600

    Changed view for parser

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,5 @@ from .views import EasyPDF, CategoryDetailView
 
 urlpatterns = patterns('catalog.views',
     url(r'category/(?P<slug>[a-zA-Z0-9\-]+)/$', CategoryDetailView.as_view(), name='category'),
+    url(r'get-price/(?P<pk>\d+)/$', EasyPDF.as_view(), name='price')
 )
--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,7 @@
 from .models import CategoryBlock, Category, Price
 from django.views.generic import TemplateView, DetailView
 from easy_pdf.views import PDFTemplateView
+from tools.parser import Parser
 
 
 class HomeView(TemplateView):
@@ -24,8 +25,25 @@ class EasyPDF(PDFTemplateView):
     pdf_filename = 'dudu.pdf'
 
     def get_context_data(self, **kwargs):
-        return super(EasyPDF, self).get_context_data(
-            pagesize="A4",
-            title="Hi there 22222",
+        context = super(EasyPDF, self).get_context_data(
+            # pagesize="A4",
+            # title="Hi there 22222",
             **kwargs
         )
+
+        context['table'] = self.get_table()
+        context['title'] = self.price_obj.title
+        return context
+
+    def get_table(self):
+        p = Parser()
+        return p.get_table(self.price_obj.url)
+
+    def set_file_name(self):
+        self.pdf_filename = u'price-%s.pdf' % self.price_obj.pk
+
+
+    def get(self, request, *args, **kwargs):
+        self.price_obj = Price.objects.get(pk=kwargs['pk'])
+        self.set_file_name()
+        return super(EasyPDF, self).get(request, *args, **kwargs)
