e444c660dff340e938db4532f1f2cfec1ee10207
Sergey Lutkov <lutkovsv@gmail.com>
2014-07-20 12:37:08 +0600

    Added parser

---
+++ tools/parser.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+
+browser_dict = {
+    'opera': 'Opera/9.80 (X11; Linux x86_64) Presto/2.12.388 Version/12.16',
+    'chromium': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36',
+    'firefox': 'Mozilla/5.0 (X11; Linux x86_64; rv:23.0) Gecko/20100101 Firefox/23.0',
+}
+
+
+class Parser():
+
+    def test(self, text):
+        print text
+
+    def get_html(self, url):
+        request = urllib2.Request(url)
+        request.add_header('User-Agent', browser_dict['chromium'])
+        try:
+            response = urllib2.urlopen(request)
+            if response.code == 200:
+                html = response.read()
+                return BeautifulSoup(html)#, 'lxml')
+            else:
+                return None
+        except:
+            return None
+
+    def get_table(self, url):
+        page = self.get_html(url)
+        if page:
+            return page.find('table', { "class" : "price-list" })
+        else:
+            return None
