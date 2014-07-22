69f29cf8bea223d930f7647e4aa60cb3807e3bd3
Sergey Lutkov <lutkovsv@gmail.com>
2014-07-22 09:29:03 +0600

    Updated partner model

--- partners/models.py
+++ partners/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class Partner(models.Model):
     title = models.CharField(u'Название', max_length=500)
     logo = models.ImageField(u'Логотип', upload_to='partners')
-    description = models.TextField(u'Описание')
-    url = models.URLField(u'Ссылка на сайт')
+    description = models.TextField(u'Описание', blank=True)
+    url = models.URLField(u'Ссылка на сайт', blank=True)
 
     class Meta:
         verbose_name = u'Партнер'
